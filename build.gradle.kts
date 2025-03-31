df275ebe8f330427e198e08568374bf94a92c116
AliasAccount <140289748+AliasAccount@users.noreply.github.com>
2025-03-31 12:29:47 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,7 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.10" apply false
 }
+dependencies {
+    implementation("androidx.compose.material:material-icons-extended:$compose_version")
+
+}
